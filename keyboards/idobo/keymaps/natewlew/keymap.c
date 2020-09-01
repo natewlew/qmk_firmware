f0a115d666929ba0b3652c25271bb0bfcba278a1
Nathan Lewis <natewlew@gmail.com>
2020-08-31 19:57:01 -0500

    Update keymaps

--- keyboards/idobo/keymaps/natewlew/keymap.c
+++ keyboards/idobo/keymaps/natewlew/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_F1,   KC_F2,   KC_F3,     KC_6,    KC_7,    KC_8,       KC_9,     KC_0, KC_DEL,
     KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_F4,   KC_F5,   KC_F6,     KC_Y,    KC_U,    KC_I,       KC_O,     KC_P, KC_BSPC,
     KC_ESC,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_F7,   KC_F8,   KC_F9,     KC_H,    KC_J,    KC_K,       KC_L,     KC_SCLN, KC_QUOT,
-   KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_F10,  KC_F11,  KC_F12,    KC_N,    KC_M,    KC_COMM,    KC_DOT,  KC_SLSH, KC_ENT,
+   KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_F10,  KC_F11,  KC_F12,    KC_N,    KC_M,    KC_COMM,    KC_DOT,  KC_SLSH, MT(MOD_RSFT,KC_ENT) ,
    KC_LCTL,  KC_LALT, KC_LGUI,  ADJUST,   LOWER,  KC_SPC,      KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,   RAISE,    KC_LEFT,  KC_DOWN,   KC_UP, KC_RGHT
 ),
 
@@ -51,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * └──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┘
  */
 [_LOWER] = LAYOUT_ortho_5x15(
-    KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______, _______, _______,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-    KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______, _______, _______,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______, _______, _______,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+    KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______, _______, _______,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
     KC_CAPS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______, _______, _______, _______,  _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
     _______, _______, _______, _______,  _______,_______, _______, _______, _______,  _______, _______, _______, KC_DOT,  _______,  _______,
     _______, _______, _______, _______,  _______,_______, _______, _______, _______,  _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
@@ -72,8 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * └──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┘
  */
 [_RAISE] = LAYOUT_ortho_5x15(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, _______,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, _______,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, _______,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, _______,  _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, KC_DOT,  _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_ADJUST] = LAYOUT_ortho_5x15(
     KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, RESET,   _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, KC_BSPC,
+  _______, RESET,   _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, KC_DEL,
   _______, _______, _______, _______, _______, AG_NORM, _______, _______, _______,  AG_SWAP, _______, _______, _______, _______, _______,
   EEP_RST, _______, RGB_HUD, RGB_SAD, RGB_VAD,  BL_DEC, _______, _______, _______,  _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
   _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD
@@ -148,4 +148,13 @@ void matrix_scan_user(void) {
 
 void led_set_user(uint8_t usb_led) {
 
+}
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MT(MOD_RSFT,KC_ENT):
+            return TAPPING_TERM + 100;
+        default:
+            return TAPPING_TERM;
+    }
 }
